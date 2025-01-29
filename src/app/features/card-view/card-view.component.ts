87fcaeb39edd230d9e73ae4c816f4aa75fd08508
Vlad Ostk <vladjavaacc@gmail.com>
2025-01-28 21:40:50 -0600

    create card view feature component create grid view feature component

---
+++ src/app/features/card-view/card-view.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-card-view',
+	imports: [],
+	templateUrl: './card-view.component.html',
+	styleUrl: './card-view.component.scss',
+})
+export class CardViewComponent {}
