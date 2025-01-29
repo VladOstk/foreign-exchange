b4ac75c1f5d0983f1e2c03398e1595ea4f2f9841
Vlad Ostk <vladjavaacc@gmail.com>
2025-01-29 00:06:59 -0600

    create currency pair manager service

---
+++ src/app/shared/services/currency-pair-manager/currency-pair-manager.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, signal } from '@angular/core';
+import { CurrencyPair } from './models';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class CurrencyPairManagerService {
+	currencyPairs = signal<CurrencyPair[]>([
+		{
+			fromCurrencyCode: 'USD',
+			fromCurrencyName: 'United States Dollar',
+			toCurrencyCode: 'JPY',
+			toCurrencyName: 'Japanese Yen',
+			exchangeRate: '155.32700000',
+			lastRefreshed: '2025-01-29 04:41:02',
+			timeZone: 'UTC',
+			bidPrice: '155.32010000',
+			askPrice: '155.33000000',
+		},
+	]);
+}
---
+++ src/app/shared/services/currency-pair-manager/models.ts
@@ -0,0 +1,11 @@
+export type CurrencyPair = {
+	fromCurrencyCode: string; // 'USD';
+	fromCurrencyName: string; // 'United States Dollar';
+	toCurrencyCode: string; // 'JPY';
+	toCurrencyName: string; // 'Japanese Yen';
+	exchangeRate: string; // '155.32700000';
+	lastRefreshed: string; // '2025-01-29 04:41:02';
+	timeZone: string; // 'UTC';
+	bidPrice: string; // '155.32010000';
+	askPrice: string; // '155.33000000';
+};
