18feb32d5b2c160395f330a6fc8bfbc26a555a93
Vlad Ostk <vladjavaacc@gmail.com>
2025-01-28 21:43:57 -0600

    create routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
-import { Routes } from '@angular/router';
+import { Route, Routes } from '@angular/router';
+import { CardViewComponent } from './features/card-view/card-view.component';
 
-export const routes: Routes = [];
+export const cardRoutePath = 'card';
+export const cardRoute: Route = {
+	path: cardRoutePath,
+	title: 'Card',
+	component: CardViewComponent,
+};
+
+export const gridRoutePath = 'grid';
+export const gridRoute: Route = {
+	path: gridRoutePath,
+	title: 'Grid',
+	loadComponent: () =>
+		import('./features/grid-view/grid-view.component').then(
+			(m) => m.GridViewComponent,
+		),
+};
+
+export const routes: Routes = [
+	{ path: '', redirectTo: 'card', pathMatch: 'full' },
+	cardRoute,
+	gridRoute,
+	{
+		path: '**',
+		loadComponent: () =>
+			import('./features/page-not-found/page-not-found.component').then(
+				(m) => m.PageNotFoundComponent,
+			),
+	},
+];
