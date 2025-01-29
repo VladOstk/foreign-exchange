86f17a37e62e5b8fa5f27c12e135581a201eada3
Vlad Ostk <vladjavaacc@gmail.com>
2025-01-29 12:45:04 -0600

    validate api data schema

--- src/app/shared/infrastructure/alpha-vantage-api/alpha-vantage-api.service.ts
+++ src/app/shared/infrastructure/alpha-vantage-api/alpha-vantage-api.service.ts
@@ -1,8 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { map } from 'rxjs';
 
 import { environment } from '../../../../environments/environment';
-import { AlphaVantageGetResponse } from './models';
+import {
+	AlphaVantageGetResponse,
+	AlphaVantageGetResponseSchema,
+} from './models';
 
 @Injectable({
 	providedIn: 'root',
@@ -11,14 +15,53 @@ export class AlphaVantageApiService {
 	private _httpClient = inject(HttpClient);
 
 	get(fromCurrency: string, toCurrency: string) {
-		return this._httpClient.get<AlphaVantageGetResponse>(
-			`${environment.apiConfigs.alphaVantage.url}`,
-			{
-				params: {
-					from_currency: fromCurrency,
-					to_currency: toCurrency,
+		return this._httpClient
+			.get<AlphaVantageGetResponse>(
+				`${environment.apiConfigs.alphaVantage.url}`,
+				{
+					params: {
+						from_currency: fromCurrency,
+						to_currency: toCurrency,
+					},
 				},
+			)
+			.pipe(
+				map((data) => {
+					//TODO: remove when api works consistently
+
+					if (this.validateSchema(data)) {
+						return data;
+					}
+
+					return this.returnDummyData();
+				}),
+			);
+	}
+
+	private validateSchema(data: unknown): data is AlphaVantageGetResponse {
+		const parseResult = AlphaVantageGetResponseSchema.safeParse(data);
+
+		if (parseResult.success === false) {
+			//collect logs and send to a centralized logger service e.g. NewRelic/CloudWatch
+			return false;
+		}
+
+		return true;
+	}
+
+	private returnDummyData(): AlphaVantageGetResponse {
+		return {
+			'Realtime Currency Exchange Rate': {
+				'1. From_Currency Code': 'TST',
+				'2. From_Currency Name': 'United States Dollar',
+				'3. To_Currency Code': 'DTA',
+				'4. To_Currency Name': 'Japanese Yen',
+				'5. Exchange Rate': '155.32700000',
+				'6. Last Refreshed': '2025-01-29 04:41:02',
+				'7. Time Zone': 'UTC',
+				'8. Bid Price': '155.32010000',
+				'9. Ask Price': '155.33000000',
 			},
-		);
+		};
 	}
 }
--- src/app/shared/infrastructure/alpha-vantage-api/models.ts
+++ src/app/shared/infrastructure/alpha-vantage-api/models.ts
@@ -1,13 +1,19 @@
-export type AlphaVantageGetResponse = {
-	'Realtime Currency Exchange Rate': {
-		'1. From_Currency Code': string; // 'USD';
-		'2. From_Currency Name': string; // 'United States Dollar';
-		'3. To_Currency Code': string; // 'JPY';
-		'4. To_Currency Name': string; // 'Japanese Yen';
-		'5. Exchange Rate': string; // '155.32700000';
-		'6. Last Refreshed': string; // '2025-01-29 04:41:02';
-		'7. Time Zone': string; // 'UTC';
-		'8. Bid Price': string; // '155.32010000';
-		'9. Ask Price': string; // '155.33000000';
-	};
-};
+import { z } from 'zod';
+
+export const AlphaVantageGetResponseSchema = z.object({
+	'Realtime Currency Exchange Rate': z.object({
+		'1. From_Currency Code': z.string(), // 'USD';
+		'2. From_Currency Name': z.string(), // 'United States Dollar';
+		'3. To_Currency Code': z.string(), // 'JPY';
+		'4. To_Currency Name': z.string(), // 'Japanese Yen';
+		'5. Exchange Rate': z.string(), // '155.32700000';
+		'6. Last Refreshed': z.string(), // '2025-01-29 04:41:02';
+		'7. Time Zone': z.string(), // 'UTC';
+		'8. Bid Price': z.string(), // '155.32010000';
+		'9. Ask Price': z.string(), // '155.33000000';
+	}),
+});
+
+export type AlphaVantageGetResponse = z.infer<
+	typeof AlphaVantageGetResponseSchema
+>;
