38491340bfb3f07e16818a75d80088621053b163
Vlad Ostk <vladjavaacc@gmail.com>
2025-01-28 21:44:27 -0600

    use navigation bar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { cardRoutePath, gridRoutePath } from './app.routes';
+import { NavigationBarRoute } from './shared/components/navigation-bar/models';
+import { NavigationBarComponent } from './shared/components/navigation-bar/navigation-bar.component';
+import { AddCurrencyButtonComponent } from './shared/components/add-currency-button/add-currency-button.component';
+
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet, NavigationBarComponent, AddCurrencyButtonComponent],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'foreign-exchange';
+	protected navigationBarRoutes: NavigationBarRoute[] = [
+		{ title: 'Card', path: cardRoutePath },
+		{ title: 'Grid', path: gridRoutePath },
+	];
 }
