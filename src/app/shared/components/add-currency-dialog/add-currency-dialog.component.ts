141fae67d9efd8d9db3b09792313ab640687be0d
Vlad Ostk <vladjavaacc@gmail.com>
2025-01-29 12:45:51 -0600

    handle network errors in add currency dialog

--- src/app/shared/components/add-currency-dialog/add-currency-dialog.component.ts
+++ src/app/shared/components/add-currency-dialog/add-currency-dialog.component.ts
@@ -1,12 +1,12 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, signal, viewChild } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { AlphaVantageApiService } from '../../infrastructure/alpha-vantage-api/alpha-vantage-api.service';
-import { CurrencyPairManagerService } from '../../services/currency-pair-manager/currency-pair-manager.service';
-import { FormsModule } from '@angular/forms';
-import { CurrencyPair } from '../../services/currency-pair-manager/models';
+import { catchError, throwError } from 'rxjs';
+
+import { AddCurrencyDialogService } from './add-currency-dialog.service';
 
 @Component({
 	selector: 'app-add-currency-dialog',
@@ -23,38 +23,39 @@ import { CurrencyPair } from '../../services/currency-pair-manager/models';
 })
 export class AddCurrencyDialogComponent {
 	protected currencyPair = signal<string>('');
+	protected networkError = signal<boolean>(false);
+	protected formControl = viewChild.required<NgModel>('control');
 
-	private _alphaVantageApiService = inject(AlphaVantageApiService);
-	private _currencyPairManagerService = inject(CurrencyPairManagerService);
+	private _addCurrencyDialogService = inject(AddCurrencyDialogService);
 	private _dialog = inject(MatDialog);
 
 	onAddClick() {
+		if (this.formControl().invalid) {
+			return;
+		}
+
+		this.updateData();
+	}
+
+	private updateData() {
+		this.networkError.set(false);
+
 		const fromCurrencyCode = this.currencyPair().slice(0, 3);
 		const toCurrencyCode = this.currencyPair().slice(3);
 
-		this._alphaVantageApiService
-			.get(fromCurrencyCode, toCurrencyCode)
-			.subscribe((data) => {
-				const currentPairs = this._currencyPairManagerService.currencyPairs();
-				const currencyData = data['Realtime Currency Exchange Rate'];
-				const newCurrencyPair: CurrencyPair = {
-					fromCurrencyCode: currencyData['1. From_Currency Code'],
-					fromCurrencyName: currencyData['2. From_Currency Name'],
-					toCurrencyCode: currencyData['3. To_Currency Code'],
-					toCurrencyName: currencyData['4. To_Currency Name'],
-					exchangeRate: currencyData['5. Exchange Rate'],
-					lastRefreshed: currencyData['6. Last Refreshed'],
-					timeZone: currencyData['7. Time Zone'],
-					bidPrice: currencyData['8. Bid Price'],
-					askPrice: currencyData['9. Ask Price'],
-				};
+		this._addCurrencyDialogService
+			.getData(fromCurrencyCode, toCurrencyCode)
+			.pipe(
+				catchError(() => {
+					this.networkError.set(true);
 
-				this._currencyPairManagerService.currencyPairs.set([
-					...currentPairs,
-					newCurrencyPair,
-				]);
-
-				this._dialog.closeAll();
+					return throwError(() => null);
+				}),
+			)
+			.subscribe({
+				next: () => {
+					this._dialog.closeAll();
+				},
 			});
 	}
 }
---
+++ src/app/shared/components/add-currency-dialog/add-currency-dialog.service.ts
@@ -0,0 +1,55 @@
+import { inject, Injectable } from '@angular/core';
+import { tap } from 'rxjs';
+
+import { CurrencyPair } from '../../services/currency-pair-manager/models';
+import { AlphaVantageGetResponse } from '../../infrastructure/alpha-vantage-api/models';
+import { CurrencyPairManagerService } from '../../services/currency-pair-manager/currency-pair-manager.service';
+import { AlphaVantageApiService } from '../../infrastructure/alpha-vantage-api/alpha-vantage-api.service';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class AddCurrencyDialogService {
+	private _currencyPairManagerService = inject(CurrencyPairManagerService);
+	private _alphaVantageApiService = inject(AlphaVantageApiService);
+
+	getData(fromCurrencyCode: string, toCurrencyCode: string) {
+		return this._alphaVantageApiService
+			.get(fromCurrencyCode, toCurrencyCode)
+			.pipe(
+				tap((data) => {
+					this.updateData(data);
+				}),
+			);
+	}
+
+	private updateData(alphaVantageResponse: AlphaVantageGetResponse) {
+		const newCurrencyPair =
+			this.adaptAlphaVantageToCurrencyPair(alphaVantageResponse);
+
+		const currentPairs = this._currencyPairManagerService.currencyPairs();
+		this._currencyPairManagerService.currencyPairs.set([
+			...currentPairs,
+			newCurrencyPair,
+		]);
+	}
+
+	private adaptAlphaVantageToCurrencyPair(
+		alphaVantageResponse: AlphaVantageGetResponse,
+	): CurrencyPair {
+		const currencyData =
+			alphaVantageResponse['Realtime Currency Exchange Rate'];
+
+		return {
+			fromCurrencyCode: currencyData['1. From_Currency Code'],
+			fromCurrencyName: currencyData['2. From_Currency Name'],
+			toCurrencyCode: currencyData['3. To_Currency Code'],
+			toCurrencyName: currencyData['4. To_Currency Name'],
+			exchangeRate: currencyData['5. Exchange Rate'],
+			lastRefreshed: currencyData['6. Last Refreshed'],
+			timeZone: currencyData['7. Time Zone'],
+			bidPrice: currencyData['8. Bid Price'],
+			askPrice: currencyData['9. Ask Price'],
+		};
+	}
+}
