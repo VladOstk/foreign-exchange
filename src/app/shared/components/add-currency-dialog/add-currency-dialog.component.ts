4ee0adb918afcc206342d4e683e13b44b15f28d7
Vlad Ostk <vladjavaacc@gmail.com>
2025-01-29 00:07:45 -0600

    add currency dialog

--- src/app/shared/components/add-currency-dialog/add-currency-dialog.component.ts
+++ src/app/shared/components/add-currency-dialog/add-currency-dialog.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { AlphaVantageApiService } from '../../infrastructure/alpha-vantage-api/alpha-vantage-api.service';
+import { CurrencyPairManagerService } from '../../services/currency-pair-manager/currency-pair-manager.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
 	selector: 'app-add-currency-dialog',
-	imports: [],
+	imports: [
+		MatDialogModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatButtonModule,
+		FormsModule,
+	],
 	standalone: true,
 	templateUrl: './add-currency-dialog.component.html',
 	styleUrl: './add-currency-dialog.component.scss',
 })
-export class AddCurrencyDialogComponent {}
+export class AddCurrencyDialogComponent {
+	protected currencyPair = signal<string>('');
+
+	private _alphaVantageApiService = inject(AlphaVantageApiService);
+	private _currencyPairManagerService = inject(CurrencyPairManagerService);
+
+	onAddClick() {
+		const fromCurrencyCode = this.currencyPair().slice(0, 3);
+		const toCurrencyCode = this.currencyPair().slice(3);
+
+		this._alphaVantageApiService
+			.get(fromCurrencyCode, toCurrencyCode)
+			.subscribe((data) => {
+				const currentPairs = this._currencyPairManagerService.currencyPairs();
+				const currencyData = data['Realtime Currency Exchange Rate'];
+
+				currentPairs.push({
+					fromCurrencyCode: currencyData['1. From_Currency Code'],
+					fromCurrencyName: currencyData['2. From_Currency Name'],
+					toCurrencyCode: currencyData['3. To_Currency Code'],
+					toCurrencyName: currencyData['4. To_Currency Name'],
+					exchangeRate: currencyData['5. Exchange Rate'],
+					lastRefreshed: currencyData['6. Last Refreshed'],
+					timeZone: currencyData['7. Time Zone'],
+					bidPrice: currencyData['8. Bid Price'],
+					askPrice: currencyData['9. Ask Price'],
+				});
+			});
+	}
+}
