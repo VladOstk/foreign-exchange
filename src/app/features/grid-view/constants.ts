b5be9eb81e85025298ff91105a933ed2bcd9d54b
Vlad Ostk <vladjavaacc@gmail.com>
2025-01-29 00:26:39 -0600

    format date in grid

--- src/app/features/grid-view/constants.ts
+++ src/app/features/grid-view/constants.ts
@@ -1,4 +1,5 @@
 import { type ColDef } from '@ag-grid-community/core';
+
 import { GridViewRowData } from './models';
 
 export const gridViewColDefs: ColDef<GridViewRowData>[] = [
@@ -15,5 +16,18 @@ export const gridViewColDefs: ColDef<GridViewRowData>[] = [
 	},
 	{ field: 'bidPrice', flex: 1 },
 	{ field: 'askPrice', flex: 1 },
-	{ field: 'lastUpdated', flex: 1 },
+	{
+		field: 'lastUpdated',
+		flex: 1,
+		valueFormatter: (params) => {
+			return new Intl.DateTimeFormat('en-US', {
+				month: '2-digit',
+				day: '2-digit',
+				year: 'numeric',
+				hour: '2-digit',
+				minute: '2-digit',
+				hour12: true,
+			}).format(new Date(params.value));
+		},
+	},
 ];
